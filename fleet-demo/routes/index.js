3bd19fc259321ab3355858d98ea63bda55507ddf
bala <balavaradarajan@1185508.no-reply.drupal.org>
2019-08-28 21:09:05 +0530

    Removing unwanted projects from tulf branch

--- fleet-demo/routes/index.js
+++
@@ -1,2 +0,0 @@
-exports.fleetReportService = require('./FleetReportService');
-exports.homeService = require('./homeService');
--- pet-clinic/frontend/src/app/shared/modules/models/dndblacklist.ts
+++
@@ -1,4 +0,0 @@
-export interface DndBlacklist {
-    msisdn: string;
-    createdDate: Date;
-}
