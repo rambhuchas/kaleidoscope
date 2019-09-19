edda5cfd4217009907f8e3ce46323a6eaef6f344
bala <balavaradarajan@1185508.no-reply.drupal.org>
2019-09-19 18:24:26 +0530

    adding octcms to tulf.

--- install-master/storage/framework/classes.php
+++ install-master/storage/framework/classes.php
@@ -3,4 +3,7 @@
   'backend\\Controllers\\auth' => 'modules\\backend\\controllers\\auth.php',
   'backend\\Controllers\\index' => 'modules\\backend\\controllers\\index.php',
   'cms\\Controllers\\index' => 'modules\\cms\\controllers\\index.php',
+  'system\\Controllers\\settings' => 'modules\\system\\controllers\\settings.php',
+  'system\\Controllers\\updates' => 'modules\\system\\controllers\\updates.php',
+  'backend\\Controllers\\media' => 'modules\\backend\\controllers\\media.php',
 );
