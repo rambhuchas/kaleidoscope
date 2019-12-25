f4185be026e9b2267bfb884875105b45280f858f
Bhuvana Mohan Kan <5145362-kbhuvanamohan@users.noreply.gitlab.com>
2019-12-25 13:35:16 +0530

    Removing fleet demo

--- fleet-demo/service/config.js
+++
@@ -1,9 +0,0 @@
-/******************************************************************************************/
-// Filename: config.js
-// Created By: Team@EDC
-// Description: Configuration vars
-/*******************************************************************************************/
-module.exports = {
-	PORT: process.env.PORT || 3001,
-	NODE_ENV: process.env.NODE_ENV || 'development'
-}
